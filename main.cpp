f77471ac1fa493a68bf2b9672dbeedfaa44eaa63
yuqing.tyq <yuqing.tyq@alibaba-inc.com>
2018-09-15 20:28:08 +0800

    Modify.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+// xxx.cpp: 定义控制台应用程序的入口点。
+// don't kid me.
+
+#include "stdafx.h"
+
+
 //
 //  main.cpp
 //  EECS project
@@ -17,126 +23,59 @@ string pluralize(string singular, string plural, int Numbatch) {
     return plural;
 }
 
+void question1();
+double respond(double x);
+double respondGoods(const double x, const std::string &unitName, const std::string &goodsName, const double price);
+
 
 int main() {                                //HI 童少！
-    int Numbatch = 0;                       //现在运行这个程序会出现：You need to make: 2 batches of cookies，对应respond(int x)
+    double Numbatch = 0.0;                       //现在运行这个程序会出现：You need to make: 2 batches of cookies，对应respond(int x)
     int people = 0;                         //我设想的是之后的方程也会输出字符串，例如：2 pounds of butter
-    void question1();                       //但respond(int x) 之后的方程无法输出字符串，而且每个方程内的数值异常。我不明白问题出在哪里
-    int respond(int x);                     //respond 1-8里 return cost...可以忽略，是我之后需要进一步完成的步骤。
-    int respond1(int x);
-    int respond2(int x);
-    int respond3(int x);
-    int respond4(int x);
-    int respond5(int x);
-    int respond6(int x);
-    int respond7(int x);
-    int respond8(int x);
+    //但respond(int x) 之后的方程无法输出字符串，而且每个方程内的数值异常。我不明白问题出在哪里
+    //respond 1-8里 return cost...可以忽略，是我之后需要进一步完成的步骤。
     
-    question1();            // ask the user for the number of people needed to serve
-    cin>> people;           // input the number of peope needed to serve
-    cout<<endl;             // start a new line to input
-    Numbatch = ceil (people / 12.0);
+    question1();
+    
+    cin >> people;           // input the number of peope needed to serve
+    cout << endl;             // start a new line to input
+    Numbatch = ceil(people / 12.0);
     respond(Numbatch);         // respond the number of batches of cookies needed to make
-    cout<< "Shopping List for \"Best Ever\" Chocolate Chip Cookies"<<endl;
-    cout<<endl;
-    respond1(Numbatch);
-    respond2(Numbatch);
-    respond3(Numbatch);
-    respond4(Numbatch);
-    respond5(Numbatch);
-    respond6(Numbatch);
-    respond7(Numbatch);
-    respond8(Numbatch);
+    cout << "Shopping List for \"Best Ever\" Chocolate Chip Cookies" << endl;
+    cout << endl;
+    respondGoods(Numbatch, "cup", "flour", 59.0f);
+    respondGoods(Numbatch, "cup", "grant sugars", 59.0f);
+    respondGoods(Numbatch, "cup", "brwon sugars", 59.0f);
+    respondGoods(Numbatch, "cup", "red sugars", 59.0f);
+    respondGoods(Numbatch, "box", "cream cheese", 59.0f);
+    respondGoods(Numbatch, "dozen", "eggs", 59.0f);
+    respondGoods(Numbatch, "oz", "vanilla", 59.0f);
+    respondGoods(Numbatch, "cup", "chickens", 59.0f);
     
     return 0;
 }
 
-void question1(){
-    cout<<"How many people do you need to serve?"<<endl;
+void question1() {
+    cout << "How many people do you need to serve?" << endl;
 }
 
-int respond(int x){
-    cout<<"You need to make: ";
-    cout << x << " "<< pluralize("batch", "batches", x) << " of cookies" << endl;
+double respond(const double x) {
+    cout << "You need to make: ";
+    cout << x << " " << pluralize("batch", "batches", static_cast<int>(x)) << " of cookies" << endl;
     return 0;
 }
 
-int respond1(int x){
-    const double flour = 0.75;
-    int numflour = ceil(flour * x);
-    cout<<numflour << " "<< pluralize("cup", "cups", numflour) << " of flour" << endl;
-    float costflour = (2.69/20) * numflour;
+double respondGoods(const double x, const std::string &unitName, const std::string &goodsName, const double price)
+{
+    const double goods = 0.75;
+    const double numflour = ceil(goods * x);
+    std::string unit = unitName;
+    if (numflour > 1.0)
+    {
+        unit.append("s");
+    }
     
-    return costflour;
-}
-
-int respond2(int x){
-    const double gransugar = 0.75;
-    int numgransugar = ceil(gransugar * x);
-    cout<<numgransugar << " "<< pluralize("cup", "cups", numgransugar) << " of granulated sugar" << endl;
-    float costgransugar = (3.99/10) * numgransugar;
+    std::cout << numflour << " " << unit << " of " << goodsName << std::endl;
     
-    return costgransugar;
+    double cost = price * numflour;
+    return cost;
 }
-
-
-int respond3(int x){
-    const double brownsugar = 0.75;
-    int numbrownsugar = ceil(brownsugar * x);
-    cout<<numbrownsugar << " "<< pluralize("cup", "cups", numbrownsugar) << " of brown sugar" << endl;
-    float costbrownsugar = (2.29/4.5) * numbrownsugar;
-    
-    return costbrownsugar;
-}
-
-int respond4(int x){
-    const double unsbutter = 0.5;
-    int numunsbutter = ceil(unsbutter * x);
-    cout<<numunsbutter<<" "<< pluralize("cup", "cups", numunsbutter) << " of brown sugar" << endl;
-    float costunsbutter = (2.79/2) * numunsbutter;
-    
-    return costunsbutter;
-}
-
-int respond5(int x){
-    const double CC = 0.5;
-    int numCC = ceil(CC * x);
-    cout<<numCC<<" "<< pluralize("box", "boxes", numCC) << " of Cream Cheese" << endl;
-    float costCC = 2.49 * numCC;
-    
-    return costCC;
-}
-
-int respond6(int x){
-    const double Eggs = 2;
-    int numEggs = ceil(Eggs * x);
-    cout<<numEggs<<" "<< pluralize("dozen", "dozens", numEggs) << " of Eggs" << endl;
-    float costEggs = (1.99/12) * numEggs;
-    
-    return costEggs;
-}
-
-int respond7(int x){
-    const double Van = 2;
-    int numVan = ceil(Van * x);
-    cout<<numVan<<" "<< pluralize("oz", "oz", numVan) << " of Vanilla" << endl;
-    float costVan = (6.79/12) * numVan;
-    
-    return costVan;
-}
-
-int respond8(int x){
-    const double DCC = 2;
-    int numDCC = ceil(DCC * x);
-    cout<<numDCC<<" "<< pluralize("cup", "cups", numDCC) << " of brown sugar" << endl;
-    float costDCC = (2.39/12) * numDCC;
-    
-    return costDCC;
-}
-
-
-
-
-
-
-
